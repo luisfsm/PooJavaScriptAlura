5cdfcfa9ef6328d5df19b7d835abc0ff63ff1733
luis felipe <luis.s.mendes20@gmail.com>
2021-08-22 17:39:41 -0300

    Add: torna clss Contas como class abstrata

--- Bytebank/Contas/Conta.js
+++ Bytebank/Contas/Conta.js
@@ -1,6 +1,9 @@
 import { Cliente } from "../Clientes/Cliente.js";
-export class Conta{
+export class  Conta{
     constructor(saldoInicial, cliente,agencia){
+        if(this.constructor===Conta){
+            throw new Error("Ai não meu patrão , não pode instanciar a classe Conta ")
+        }
         this._saldo = saldoInicial
         this._cliente = cliente
         this._agencia = agencia
