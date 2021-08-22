7c7cd0403bf84eeeb9eb7137fef7fdf21aaeca12
luis felipe <luis.s.mendes20@gmail.com>
2021-08-21 22:12:00 -0300

    ADD: Package.json

--- Bytebank/index.js
+++ Bytebank/index.js
@@ -1,9 +1,11 @@
 
 import { Cliente } from "./Clientes/Cliente.js";
 import { ContaCorrente } from "./ContaCorrente/ContaCorrente.js";
-
 const cliente = new Cliente();
 
+cliente.nome = "Luis Felipe";
+cliente.cpf ="1112938111";
+console.log(cliente)
 
 const contaCorrenteCLiente = new ContaCorrente();
 
